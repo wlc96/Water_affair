29b93dda8d35f79666d2304ecdf17ed370d87276
wulichuan <2959108020@qq.com>
2019-05-15 15:51:14 +0800

    设备停水数据填充

---
+++ database/seeds/EqStopsTableSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class EqChangesTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        return DB::transaction(function()
+        {
+	        $a = '9123091823456746435809123123123118429182';
+	    	$b = '1234';
+	    	$c = '345';
+	    	for ($i=0; $i < 20; $i++) 
+	    	{
+	    		$order_num = 'EQCH'.substr($a,5,10);
+	    		$order = DB::table('eq_stops')->insertGetId([
+		            'company_id' => 1,
+		            'status' => mt_rand(0,1),
+		            'type' => mt_rand(1,3),
+		            'user_id' => mt_rand(2,20),
+		            'station_id' => mt_rand(1,10),
+		            'order_num' => $order_num,
+		            'way' => mt_rand(1,2),
+		            'servicing_id' => 1,
+		            'affected_area' => '影响区域',
+		            'explain' => '说明',
+		            'admin_id' => 1,
+		            'created_at' => Carbon::now(),
+	            	'updated_at' => Carbon::now(),
+		        ]);
+
+		        DB::table('orders')->insert([
+		            'company_id' => 1,
+		            'order_id' => $order,
+		            'order_num' => $order_num,
+		            'type' => 'EqChange',
+		            'created_at' => Carbon::now(),
+	            	'updated_at' => Carbon::now(),
+		        ]);
+
+		        DB::table('events')->insert([
+		            'company_id' => 1,
+		            'order_id' => $order,
+		            'number' => $order_num,
+		            'type' => 'EqChange',
+		            'report_user_id' => '巡检人',
+		            'created_at' => Carbon::now(),
+	            	'updated_at' => Carbon::now(),
+		        ]);
+	    	}
+	    });
+    }
+}
