2b231419610080860ee3de878fa72bca489893a9
wlc96 <www_waitingxx@163.com>
2019-06-17 10:49:49 +0800

    Revert "修改微信支付回调地址"
    This reverts commit eb98d3ed075f44ed4c998a077336780b59105afc.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-	\Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -66,5 +65,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+	\Barryvdh\Cors\HandleCors::class
     ];
 }
